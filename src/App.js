54cc96b2fc743af59445293b4ef912c0bbfe5fbb
kingbaddo <wizzyblaq94@gmail.com>
2021-09-28 12:11:33 +0100

    added routes and navigation

--- src/App.js
+++ src/App.js
@@ -2,21 +2,49 @@ import './App.css';
 import { Switch, Route, Link } from "react-router-dom";
 import {Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './components/index';
+import { Navbar, Exchanges, Cryptocurrencies, HomePage, News, CryptoDetails } from './components/index';
 
 function App() {
   return (
     <div className="App">
-     <h1> Hello Bitsurfers</h1>
+    
      <div className="navbar">
         <Navbar />
      </div>
 
      <div className="main">
+       <Layout>
+         <div className="routes">
+           <Switch>
+             <Route exact path="/">
+               <HomePage/>
+             </Route>
+             <Route exact path="/cryptocurrencies">
+               <Cryptocurrencies/>
+               </Route>
+               <Route exact path="/crypto:coinId">
+               <CryptoDetails/>
+              </Route>
+             
+             <Route exact path="/exchanges">
+               <Exchanges/>
+                </Route>
+               <Route exact path="/news">
+               <News/>
+              </Route>
+              
+            
+           </Switch>
+         </div>
+       </Layout>
 
      </div>
 
-     <div className="footer"></div>
+     <div className="footer">
+       <Space>
+         <Typography.Title level={3}></Typography.Title>
+       </Space>
+     </div>
     </div>
   );
 }
