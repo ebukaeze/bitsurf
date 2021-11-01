52ca731ce9caaaef2f464ca17aea0c8105e815b2
kingbaddo <wizzyblaq94@gmail.com>
2021-11-01 22:38:57 +0100

    mobile hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from '../assets/images/logo1.png';
+import React, {useState, useEffect} from 'react';
+import logo from '../assets/images/bitlogo.png';
 
 import { Link } from 'react-router-dom';
 import {Button, Menu, Typography, Avatar, Layout } from 'antd';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@ant-design/icons'
 
 function Navbar() {
+    const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 800) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
     
     return (
         
         <div className="nav">
-            <div className="navbar-container">
+            <div className="nav-container">
                 <div className="logo-container">
                     <Link to="/">
-                    <div ><img src={logo} alt="logo" className="logo"/></div>
+                    <Avatar src={logo} size="large" className="logo"/>
+                    <Typography.Title level={3} className="logo-title"><Link to="/">Bitsurf</Link></Typography.Title>
                     </Link>
-                    {/* <Button className="menu-control-container"></Button> */}
+                <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
+
                 </div>
-                <Menu theme="dark">
-                    <Menu.Item icon={<HomeOutlined />}>
-                        <Link to="/">Home</Link>
+                { activeMenu && (
+                <Menu theme="dark" className="menu">
+                    <Menu.Item icon={<HomeOutlined/>}>
+                        <Link to="/" >Home</Link>
                     </Menu.Item>
                      <Menu.Item icon={<FundOutlined />}>
                         <Link to="/cryptocurrencies">Cryptocurrencies</Link>
@@ -32,6 +56,7 @@ function Navbar() {
                     </Menu.Item>
 
                 </Menu>
+                )}
             </div>
            
         </div>
