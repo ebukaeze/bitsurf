9b9067cbfa666c489efe75d347bef79e54202050
kingbaddo <wizzyblaq94@gmail.com>
2021-11-01 20:56:15 +0100

    filter cryptocurrencies and return search result

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect} from 'react';
 import millify from 'millify';
 import {Link} from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
+import Loader from './Loader';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const Cryptocurrencies = ({simplified}) => {
     const count = simplified ? 10 : 100;
    const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-   const [cryptos, setCryptos ] = useState([]);
+   const [cryptos, setCryptos ] = useState();
    console.log(cryptos);
     const [searchTerm, setSearchTerm ] = useState("");
      
@@ -18,7 +19,7 @@ const Cryptocurrencies = ({simplified}) => {
        setCryptos(filteredData)
     }, [cryptosList, searchTerm])
 
-   if(isFetching) return `Loading...`;
+   if(isFetching) return <Loader />;
 
     return (
         <>
@@ -27,10 +28,11 @@ const Cryptocurrencies = ({simplified}) => {
             <Input placeholder="search cryptocurrency" onChange={(e)=> setSearchTerm(e.target.value) }/>
         </div>
         )}
+       
            <Row gutter={[32, 32]} className="crypto-card-container">
               {cryptos?.map((currency) => (
                   <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-                      <Link to={`/crypto/${currency.id}`}>
+                      <Link key={currency.id} to={`/crypto/${currency.id}`}>
                           <Card 
                           title={`${currency.rank}, ${currency.name}`}
                           extra={<img className="crypto-image" src={currency.iconUrl} />
@@ -47,7 +49,7 @@ const Cryptocurrencies = ({simplified}) => {
                   </Col>
               ))}
            </Row>
-               
+             
         </>
     )
 }
